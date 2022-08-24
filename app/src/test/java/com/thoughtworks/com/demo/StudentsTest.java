9180e5f3a0d2a837c1055736fb3e2ea4b5ae55cf
lan.jiang <lan.jiang@thoughtworks.com>
2022-08-24 16:42:31 +0800

    feat: move mock data to test class level

--- app/src/test/java/com/thoughtworks/com/demo/StudentsTest.java
+++ app/src/test/java/com/thoughtworks/com/demo/StudentsTest.java
@@ -8,35 +8,27 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentsTest {
+    Student firstStu = new Student("stu1", 80);
+    Student secondStu = new Student("stu2", 90);
     @Test
     void should_find_student_with_list_successfully() {
-        Student firstStu = new Student("stu1", 80);
-        Student secondStu = new Student("stu2", 90);
-
         Students students = new Students(new ArrayList<Student>(List.of(firstStu, secondStu)));
         assertEquals(80, students.findStudentScoreInList("stu1"));
     }
 
     @Test
     void should_return_minus_one_when_find_student_fail() {
-        Student firstStu = new Student("stu1", 80);
-        Student secondStu = new Student("stu2", 90);
-
         Students students = new Students(new ArrayList<Student>(List.of(firstStu, secondStu)));
         assertEquals(-1, students.findStudentScoreInList("stu3"));
     }
 
     @Test
     void should_get_student_score_successfully() {
-        Student firstStu = new Student("stu1", 80);
-        Student secondStu = new Student("stu2", 90);
         Students students = new Students(new ArrayList<Student>(List.of(firstStu, secondStu)));
         assertEquals(80, students.getScore("stu1"));
     }
     @Test
     void should_return_minus_one_when_student_not_exist() {
-        Student firstStu = new Student("stu1", 80);
-        Student secondStu = new Student("stu2", 90);
         Students students = new Students(new ArrayList<Student>(List.of(firstStu, secondStu)));
         assertEquals(-1, students.getScore("stu3"));
     }
