73aa6d9cf4f2731541fb86c42b68ed812aa11ea8
lan.jiang <lan.jiang@thoughtworks.com>
2022-08-25 14:50:54 +0800

    refactor: optimize package import

--- app/src/test/java/com/thoughtworks/com/demo/AppTest.java
+++ app/src/test/java/com/thoughtworks/com/demo/AppTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
     @Test void appHandleStringWithSb() {
--- app/src/test/java/com/thoughtworks/com/demo/StudentsTest.java
+++ app/src/test/java/com/thoughtworks/com/demo/StudentsTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StudentsTest {
     Student firstStu;
