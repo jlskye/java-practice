1b3d0b10a35e1cbe10c13628264f357b3951e89b
lan.jiang <lan.jiang@thoughtworks.com>
2022-08-26 10:32:12 +0800

    feat: use stream localDate

--- app/src/main/java/com/thoughtworks/com/demo/Operator.java
+++ app/src/main/java/com/thoughtworks/com/demo/Operator.java
@@ -1,13 +1,21 @@
 package com.thoughtworks.com.demo;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.util.stream.Collectors.toCollection;
 
 public class Operator {
-
-
-    public ArrayList castListUpperCase(ArrayList<String> list) {
+    public List<String> castListUpperCase(ArrayList<String> list) {
         return list.stream().map(String::toUpperCase).collect(toCollection(ArrayList::new));
+    }
+
+
+    public LocalDate[] transferStringToDate(String[] time) {
+        return Arrays.stream(time)
+                .map(s -> s.replaceAll(" ", ""))
+                .map(LocalDate::parse).toArray(LocalDate[]::new);
     }
 }
--- app/src/test/java/com/thoughtworks/com/demo/OperatorTest.java
+++ app/src/test/java/com/thoughtworks/com/demo/OperatorTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.com.demo;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,5 +16,20 @@ class OperatorTest {
         ArrayList list = new ArrayList(List.of("sdd", "sdd2w"));
         ArrayList list2 = new ArrayList(List.of("SDD", "SDD2W"));
         assertThat(operator.castListUpperCase(list)).isEqualTo(list2);
+    }
+
+    @Test
+    void should_parse_string_to_local_date_successfully() {
+        Operator operator = new Operator();
+        String[] time = new String[] {" 2019-12-31 ", "2020 - 01-09 ", "2020- 05 - 01 ", "2022 - 02 - 01",
+                " 2025-01 -01" };
+        LocalDate date1 = LocalDate.of(2019, 12, 31);
+        LocalDate date2 = LocalDate.of(2020, 01, 9);
+        LocalDate date3 = LocalDate.of(2020, 05, 1);
+        LocalDate date4 = LocalDate.of(2022, 02, 1);
+        LocalDate date5 = LocalDate.of(2025, 01, 1);
+
+        assertThat(operator.transferStringToDate(time)).isEqualTo(new LocalDate[]{
+                date1, date2,date3,date4,date5});
     }
 }
